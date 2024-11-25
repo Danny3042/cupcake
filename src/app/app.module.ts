66e77b147bc07629a3d095d701910309c4639d1a
Danny3042 <danielramzani@gmail.com>
2024-11-25 20:44:57 +0000

    fixing issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-import {ProductDetailsComponent} from './product-details/product-details.component';
-import {ProductListComponent} from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import {ProductListComponent} from './product-list/product-list.component';
     MatIconModule,
     MatToolbarModule,
     MatListModule,
+    MatSidenavModule,
   ],
   providers: [],
 })
