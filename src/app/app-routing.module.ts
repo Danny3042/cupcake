3149743315b922e1292314d69d3fb819ce26f0fd
Daniel Ramzani <danielramzani@gmail.com>
2024-11-27 16:14:50 +0000

    trying to find out about navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,7 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import { HomeComponent} from './home/home.component';
-import { ProductListComponent} from './product-list/product-list.component';
-import { ProductDetailsComponent} from './product-details/product-details.component';
-import { ShoppingCartComponent} from './shopping-cart/shopping-cart.component';
+import {RouterModule} from '@angular/router';
+import {routes} from './app.routes';
 
-const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailsComponent},
-  { path: 'cart', component: ShoppingCartComponent},
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AppComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: ShoppingCartComponent },
+];
